cb26889833e43f23ee68747362878081268f81f8
Erico Almeida <erico.silva@invillia.com>
2022-10-15 06:23:46 -0400

    test: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/sign-up.spec.ts
+++ src/presentation/controllers/sign-up.spec.ts
@@ -70,5 +70,26 @@ describe('SignUpController', () => {
 
       expect(httpResponse.statusCode).toBe(expectedStatusCode)
     })
+
+    it('should return 400 if no password is provided', async () => {
+      const sut = new SignUpController()
+
+      const fakeName = faker.name.firstName
+      const fakeEmail = faker.internet.email
+
+      const httpRequest = {
+        body: {
+          name: fakeName,
+          email: fakeEmail
+        }
+      }
+
+      const httpResponse = await sut.handle(httpRequest)
+      const expectedStatusCode = 400
+      const expectedError = new MissingParamError('password')
+
+      expect(httpResponse.statusCode).toBe(expectedStatusCode)
+      expect(httpResponse.body).toEqual(expectedError)
+    })
   })
 })
