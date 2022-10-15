e9964db670729ad127a86a1dec7e78db3d997da4
Erico Almeida <erico.silva@invillia.com>
2022-10-15 00:23:48 -0400

    test: ensure SignUpController returns an error if no email is provided

--- src/presentation/controllers/sign-up.spec.ts
+++ src/presentation/controllers/sign-up.spec.ts
@@ -20,8 +20,32 @@ describe('SignUpController', () => {
 
       const httpResponse = await sut.handle(httpRequest)
       const expectedStatusCode = 400
+      const expectedError = new Error('Missing param: name')
 
       expect(httpResponse.statusCode).toBe(expectedStatusCode)
+      expect(httpResponse.body).toEqual(expectedError)
+    })
+
+    it('should return 400 if no email is provided', async () => {
+      const sut = new SignUpController()
+
+      const fakeName = faker.name.firstName
+      const fakePassword = faker.internet.password
+
+      const httpRequest = {
+        body: {
+          name: fakeName,
+          password: fakePassword,
+          passwordConfirmation: fakePassword
+        }
+      }
+
+      const httpResponse = await sut.handle(httpRequest)
+      const expectedStatusCode = 400
+      const expectedError = new Error('Missing param: email')
+
+      expect(httpResponse.statusCode).toBe(expectedStatusCode)
+      expect(httpResponse.body).toEqual(expectedError)
     })
   })
 })
