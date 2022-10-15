4a7f35f2ce58599bf6e25b9d91085dea5286f3d6
Erico Almeida <erico.silva@invillia.com>
2022-10-15 05:57:13 -0400

    refactor: ensure SignUpController.handle implements corrects interfaces

--- src/presentation/controllers/sign-up.controller.ts
+++ src/presentation/controllers/sign-up.controller.ts
@@ -1,5 +1,8 @@
+import { HttpRequestProtocol } from '../protocols/http-request.protocol'
+import { HttpResponseProtocol } from '../protocols/http-response.protocol'
+
 export class SignUpController {
-  public async handle (httpRequest: any): Promise<any> {
+  public async handle (httpRequest: HttpRequestProtocol): Promise<HttpResponseProtocol> {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
