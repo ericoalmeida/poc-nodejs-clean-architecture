e1b133a763d6f4242beba8d7dadb5b96a5ce1656
Erico Almeida <erico.silva@invillia.com>
2022-10-20 00:49:15 -0400

    refactor: move sut creation to a factory

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -1,18 +1,33 @@
 import { faker } from '@faker-js/faker'
 import { EncrypterProtocol } from 'src/data/protocols/encrypter.protocol'
+import { AddAccountUseCase } from 'src/domain/use-cases/add-account.usecase'
 import { DbAddAccountUseCase } from './db-add-account.usecase'
+
+interface SutTypes {
+  encrypterStub: EncrypterProtocol
+  sut: AddAccountUseCase
+}
+
+const makeSut = (): SutTypes => {
+  class EncrypterStub implements EncrypterProtocol {
+    async encrypt (password: string): Promise<string> {
+      return await new Promise(resolve => resolve(faker.internet.password()))
+    }
+  }
+
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccountUseCase(encrypterStub)
+
+  return {
+    encrypterStub,
+    sut
+  }
+}
 
 describe('DbAddAccount', () => {
   describe('#add', () => {
     it('Should call Encrypter with correct password', async () => {
-      class EncrypterStub implements EncrypterProtocol {
-        async encrypt (password: string): Promise<string> {
-          return await new Promise(resolve => resolve(faker.internet.password()))
-        }
-      }
-
-      const encrypterStub = new EncrypterStub()
-      const sut = new DbAddAccountUseCase(encrypterStub)
+      const { sut, encrypterStub } = makeSut()
 
       const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
 
