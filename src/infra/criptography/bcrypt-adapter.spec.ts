f54d4a9dccdbcc0984105d00094c640fd6609365
Erico Almeida <erico.silva@invillia.com>
2022-10-22 05:49:37 -0400

    test: ensure BCryptAdapter returns a hashed password on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -2,6 +2,14 @@ import { faker } from '@faker-js/faker'
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt.adapter'
 
+const HASHED_PASSWORD_MOCK = faker.internet.password()
+
+jest.mock('bcrypt', () => ({
+  async hashSync (): Promise<string> {
+    return await new Promise(resolve => resolve(HASHED_PASSWORD_MOCK))
+  }
+}))
+
 describe('BCryptAdapter', () => {
   describe('#encrypt', () => {
     it('Should calls bcrypt with correct password', async () => {
@@ -17,6 +25,18 @@ describe('BCryptAdapter', () => {
 
       expect(hashSpy).toHaveBeenCalledTimes(expectedCalledTimes)
       expect(hashSpy).toHaveBeenCalledWith(fakePassword, bcryptSalt)
+    })
+
+    it('Should returns hashed password on success', async () => {
+      const bcryptSalt = 12
+
+      const sut = new BcryptAdapter(bcryptSalt)
+
+      const fakePassword = faker.internet.password()
+
+      const hashedPassword = await sut.encrypt(fakePassword)
+
+      expect(hashedPassword).toBe(HASHED_PASSWORD_MOCK)
     })
   })
 })
