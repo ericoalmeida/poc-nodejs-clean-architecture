a51f5935bd93a4528730ab66953dda9613e85605
Erico Almeida <erico.silva@invillia.com>
2022-10-23 22:02:53 -0400

    test: ensure AccountRepository returns an account on success

---
+++ src/infra/db/mongodb/account/account.repository.spec.ts
@@ -0,0 +1,36 @@
+import { faker } from '@faker-js/faker'
+import { AddAccountRepositoryProtocol } from 'src/data/protocols/add-account-repository.protocol'
+import { MongoHelper } from '../helpers/mongo.helper'
+import { AccountRepository } from './account.repository'
+
+const makeSut = (): AddAccountRepositoryProtocol => {
+  return new AccountRepository()
+}
+
+describe('Account Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(String(process.env.MONGO_URL))
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('should insert a doc into collection', async () => {
+    const sut = makeSut()
+
+    const accountData = {
+      name: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    }
+
+    const accountCreated = await sut.add(accountData)
+
+    expect(accountCreated).toBeTruthy()
+    expect(accountCreated.id).toBeTruthy()
+    expect(accountCreated.name).toBe(accountData.name)
+    expect(accountCreated.email).toBe(accountData.email)
+    expect(accountCreated.password).toBe(accountData.password)
+  })
+})
