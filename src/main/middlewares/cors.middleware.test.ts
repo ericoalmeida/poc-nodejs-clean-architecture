6d3dd8f01972a7fe4712331953c88ebd0bce76c1
Erico Almeida <erico.silva@invillia.com>
2022-10-23 23:59:57 -0400

    test: ensure CORS is enabled

---
+++ src/main/middlewares/cors.middleware.test.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+import request from 'supertest'
+
+import { app } from '../config/app'
+
+describe('CORS Middleware', () => {
+  it('Should enable CORS', async () => {
+    const uriTest = '/test'
+
+    app.get(uriTest, (request: Request, response: Response) => {
+      response.send()
+    })
+
+    await request(app)
+      .get(uriTest)
+      .send()
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+})
