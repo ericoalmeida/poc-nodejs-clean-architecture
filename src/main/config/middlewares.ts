36c54d6b0393e914839b0c428aaf13ed075d457a
Erico Almeida <erico.silva@invillia.com>
2022-10-23 23:59:17 -0400

    feat: ensure CORS is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,9 @@
 import { Express } from 'express'
+
 import { bodyParser } from '../middlewares/body-parser.middleware'
+import { cors } from '../middlewares/cors.middleware'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
