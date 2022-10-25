0cbf50733f5e6dc70cca011edeacd0ec3d8b5c59
Erico Almeida <erico.silva@invillia.com>
2022-10-24 21:09:08 -0400

    test: ensure default content-type as json

---
+++ src/main/middlewares/content-type.middleware.test.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express'
+import request from 'supertest'
+
+import { app } from '../config/app'
+
+describe('Content-Type Middleware', () => {
+  it('Should return default content-type as json', async () => {
+    const uriTest = '/test'
+
+    app.get(uriTest, (request: Request, response: Response) => {
+      response.send()
+    })
+
+    await request(app)
+      .get(uriTest)
+      .send()
+      .expect('content-type', /json/)
+  })
+
+  it('Should return content-type as xml', async () => {
+    const uriTest = '/test/xml'
+
+    app.get(uriTest, (request: Request, response: Response) => {
+      response.type('xml')
+      response.send()
+    })
+
+    await request(app)
+      .get(uriTest)
+      .send()
+      .expect('content-type', /xml/)
+  })
+})
