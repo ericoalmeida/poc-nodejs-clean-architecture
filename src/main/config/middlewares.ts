6890e5c0d5c6f998f099e9e86441f706b41b3ef4
Erico Almeida <erico.silva@invillia.com>
2022-10-24 21:08:45 -0400

    feat: ensure default content-type as json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,9 +1,11 @@
 import { Express } from 'express'
 
 import { bodyParser } from '../middlewares/body-parser.middleware'
+import { contentType } from '../middlewares/content-type.middleware'
 import { cors } from '../middlewares/cors.middleware'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
