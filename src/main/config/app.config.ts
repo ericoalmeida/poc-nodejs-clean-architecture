bf6f0cabb6bf6980a2aaaace4c1f566e95e69d7b
Erico Almeida <erico.silva@invillia.com>
2022-10-25 07:24:45 -0400

    refactor: rename middleware config file

--- src/main/config/app.config.ts
+++ src/main/config/app.config.ts
@@ -1,5 +1,5 @@
 import express, { Express } from 'express'
-import setupMiddlewares from './middlewares'
+import setupMiddlewares from './middlewares.config'
 import setupRoutes from './routes'
 
 const app: Express = express()
